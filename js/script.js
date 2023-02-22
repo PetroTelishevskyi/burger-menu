df6b9162fd4f3f4a850c3b8e1a89abae7dd47341
Petro Telishevskyi <ptelishewsky@gmail.com>
2023-02-22 17:46:12 +0200

    add feature close menu when click menu item

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 // Init variables for burger menu, menu body add menu links
 const iconMenu = document.querySelector('.icon-menu');
 const bodyMenu = document.querySelector('.menu');
-const linksMenu = document.querySelectorAll('.menu__links');
+const linksMenu = document.querySelectorAll('.menu__link');
 
 //  functon open and close menu using burger menu
 function openAndCloseMenu() {
     if (iconMenu) {
         iconMenu.addEventListener('click', () => {
-            iconMenu.classList.toggle('__active');
-            bodyMenu.classList.toggle('__active');
-            document.body.classList.toggle('__lock');            
+            iconMenu.classList.toggle('_active');
+            bodyMenu.classList.toggle('_active');
+            document.body.classList.toggle('_lock');            
+        });
+    };
+
+    if (linksMenu.length>0) {
+        linksMenu.forEach(linkMenu => {
+            linkMenu.addEventListener('click', () => {
+                if(iconMenu.classList.contains('_active')) {
+                    iconMenu.classList.remove('_active');
+                    bodyMenu.classList.remove('_active');
+                    document.body.classList.remove('_lock');
+                };
+            });
         });
     };
 };
